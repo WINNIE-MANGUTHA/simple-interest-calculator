2a3b3d28c9653f707e7e0addc17a17138586161e
WINNIE-MANGUTHA <winnibenson@gmail.com>
2019-02-12 13:18:36 +0300

    calculator

--- app/src/main/java/dev/wixx/calculator/MainActivity.java
+++ app/src/main/java/dev/wixx/calculator/MainActivity.java
@@ -1,5 +1,7 @@
 package dev.wixx.calculator;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,11 +11,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGetResult;
+    Button btnGetResult, btnRetrieve, btnClear;
     EditText txtPrinciple, txtTime, txtRate;
     TextView result;
+
+    SharedPreferences preferenceDatabase;
+    SharedPreferences.Editor editor;
+
+    ArrayList<String> preferencesResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
         txtRate = (EditText) findViewById(R.id.txtRate);
         txtTime = (EditText) findViewById(R.id.txtTime);
         result = (TextView) findViewById(R.id.txtResult);
-
+        btnClear = (Button)findViewById(R.id.btnClear);
         btnGetResult = (Button) findViewById(R.id.btnResult);
+        btnRetrieve = (Button)findViewById(R.id.btnRetrieve);
+        preferenceDatabase = getSharedPreferences("data", Context.MODE_PRIVATE);
 
         btnGetResult.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +51,32 @@ public class MainActivity extends AppCompatActivity {
                 double rate = Double.parseDouble(txtRate.getText().toString());
                 double time = Double.parseDouble(txtTime.getText().toString());
 
+
                 result.setText(String.valueOf(calculateInterest(principle,rate,time)));
+                saveValuesPreferences(principle,rate,time,calculateInterest(principle,rate,time));
             }
         });
 
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkIfNumberisEmpty(txtPrinciple.getText().toString(),txtRate.getText().toString(),txtTime.getText().toString())){
+                    Toast.makeText(MainActivity.this, "Fields are Empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                clearEditTexts(txtPrinciple,txtRate,txtTime,result);
+            }
+        });
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtPrinciple.setText(fetchValues().get(0));
+                txtRate.setText(fetchValues().get(1));
+                txtTime.setText(fetchValues().get(2));
+                result.setText(fetchValues().get(3));
 
+            }
+        });
 
     }
 
@@ -56,5 +88,34 @@ public class MainActivity extends AppCompatActivity {
 
         return principle * (rate / 100) * time;
     }
+    private void clearEditTexts(EditText txtPrinciple, EditText txtRate,EditText txtTime, TextView txtResult){
+        txtPrinciple.setText("");
+        txtRate.setText("");
+        txtTime.setText("");
+        txtResult.setText("");
+    }
+    private void saveValuesPreferences(double principle,double rate,double time, double result){
+        editor = preferenceDatabase.edit();
+        editor.putString("principle",String.valueOf(principle));
+        editor.putString("rate",String.valueOf(rate));
+        editor.putString("time",String.valueOf(time));
+        editor.putString("result",String.valueOf(result));
+        editor.apply();
+    }
+
+    private ArrayList<String> fetchValues(){
+        preferencesResult = new ArrayList<>();
+        String principle =preferenceDatabase.getString("principle","null");
+        String rate = preferenceDatabase.getString("rate","null");
+        String time = preferenceDatabase.getString("time","null");
+        String result = preferenceDatabase.getString("result","null");
+        preferencesResult.add(principle);
+        preferencesResult.add(rate);
+        preferencesResult.add(time);
+        preferencesResult.add(result);
+
+        return preferencesResult;
+    }
+
 
 }
